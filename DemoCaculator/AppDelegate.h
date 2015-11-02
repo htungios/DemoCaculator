0188dce7949ba952752213137419fe7e9a1ca4d3
Hoang Ngoc Tung <tungmdc55@gmail.com>
2015-11-02 15:17:35 +0700

    Initial Commit

---
+++ DemoCaculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoCaculator
+//
+//  Created by Hoang Ngoc Tung on 02/11/2015.
+//  Copyright (c) Năm 2015 HoangNgocTung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoCaculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoCaculator
+//
+//  Created by Hoang Ngoc Tung on 02/11/2015.
+//  Copyright (c) Năm 2015 HoangNgocTung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
