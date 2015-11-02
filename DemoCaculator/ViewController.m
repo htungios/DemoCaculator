4f8423803cbb9200ec3f9f3670c4e2f2b5103c44
Hoang Ngoc Tung <tungmdc55@gmail.com>
2015-11-02 17:44:46 +0700

    First Commit

--- DemoCaculator/ViewController.m
+++ DemoCaculator/ViewController.m
@@ -9,19 +9,117 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *txtkq;
 
 @end
 
 @implementation ViewController
+{
+    NSString *string;
+    NSString *str;
+    float so1;
+    float so2;
+    float kq;
+    }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    string = @"";
+}
+- (IBAction)num1:(id)sender {
+    string = [string stringByAppendingString:@"1"];
+    _txtkq.text = string;
+}
+- (IBAction)num2:(id)sender {
+    string = [string stringByAppendingString:@"2"];
+    _txtkq.text = string;
+}
+- (IBAction)num3:(id)sender {
+    string = [string stringByAppendingString:@"3"];
+    _txtkq.text = string;
+}
+- (IBAction)num4:(id)sender {
+    string = [string stringByAppendingString:@"4"];
+    _txtkq.text = string;
+}
+- (IBAction)num5:(id)sender {
+    string = [string stringByAppendingString:@"5"];
+    _txtkq.text = string;
+}
+- (IBAction)num6:(id)sender {
+    string = [string stringByAppendingString:@"6"];
+    _txtkq.text = string;
+}
+- (IBAction)num7:(id)sender {
+    string = [string stringByAppendingString:@"7"];
+    _txtkq.text = string;
+}
+- (IBAction)num8:(id)sender {
+    string = [string stringByAppendingString:@"8"];
+    _txtkq.text = string;
+}
+- (IBAction)num9:(id)sender {
+    string = [string stringByAppendingString:@"9"];
+    _txtkq.text = string;
+}
+- (IBAction)num0:(id)sender {
+    string = [string stringByAppendingString:@"0"];
+    _txtkq.text = string;
+}
+- (IBAction)cong:(id)sender {
+    so1 = [string floatValue];
+    string = @"";
+    _txtkq.text = string;
+    str = @"+";
+}
+- (IBAction)tru:(id)sender {
+    so1 = string.floatValue;
+    string = @"";
+    _txtkq.text = string;
+    str = @"-";
+}
+- (IBAction)nhan:(id)sender {
+    so1 = string.floatValue;
+    string = @"";
+    _txtkq.text = string;
+   str = @"*";
+}
+- (IBAction)chia:(id)sender {
+    so1 = string.floatValue;
+    string = @"";
+    _txtkq.text = string;
+    str = @"/";
+}
+- (IBAction)bang:(id)sender {
+    string = _txtkq.text;
+    so2 = string.floatValue;
+    if ([str isEqualToString:@"+"]) {
+        kq = so1 + so2;
+        string = [NSString stringWithFormat:@"%.3f",kq];
+        _txtkq.text = string;
+    }
+    if ([str isEqualToString:@"-"]) {
+        kq = so1 - so2;
+        string = [NSString stringWithFormat:@"%.3f",kq];
+        _txtkq.text = string;
+    }
+    if ([str isEqualToString:@"*"]) {
+        kq = so1 * so2;
+        string = [NSString stringWithFormat:@"%.3f",kq];
+        _txtkq.text = string;
+    }
+    if ([str isEqualToString:@"/"]) {
+        kq = so1 / so2;
+        string = [NSString stringWithFormat:@"%.3f",kq];
+        _txtkq.text = string;
+    }
+        
+}
+- (IBAction)C:(id)sender {
+    string = @"";
+    _txtkq.text = string;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
